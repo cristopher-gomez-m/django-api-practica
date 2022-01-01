8c3c56f21617bd15f63455a10fc03b14d90a8cdd
alex <sonrisita011@hotmail.com>
2022-01-01 13:57:46 -0500

    inicializando el repositorio

---
+++ Proyecto_api/api/views.py
@@ -0,0 +1,61 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from .models import Company
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
+# Create your views here.
+
+class companyView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self,request,id=0):
+        if(id>0):
+            companies= list(Company.objects.filter(id=id).values())
+            if len(companies)>0:
+                company=companies[0]
+                datos={'message':"Success", 'company': company}
+            else:
+                datos={'message':"Compañia no encontrada"}
+            return JsonResponse(datos)
+        else:
+            companies= list(Company.objects.values())
+            if len(companies)>0:
+                datos={'message':"Success", 'companies': companies}
+            else:
+                datos={'message':"Compañias no encontradas"}
+            return JsonResponse(datos)
+
+    def post(self,request):
+        jd=json.loads(request.body)
+        Company.objects.create(name=jd['name'],website=jd['website'],foundation=jd['foundation'])
+        datos={'message':"Success"}
+        return JsonResponse(datos)
+
+    def put(self,request,id):
+        jd=json.loads(request.body)
+        companies=list(Company.objects.filter(id=id).values())
+        if len(companies)>0:
+            company= Company.objects.get(id=id)
+            company.name=jd['name']
+            company.website=jd['website']
+            company.foundation=jd['foundation']
+            company.save()
+            datos={'message':"Success"}
+        else:
+            datos={'message':"Compañias no encontradas"}
+        return JsonResponse(datos) 
+
+
+    def delete(self,request,id):
+        companies=list(Company.objects.filter(id=id).values())
+        if len(companies)>0:
+            Company.objects.filter(id=id).delete()
+            datos={'message':"Success"}
+        else:
+            datos={'message':"Compañias no encontradas"}
+        return JsonResponse(datos)
